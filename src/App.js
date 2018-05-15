17cd907a4d477b24126503096dfecbbda281caf8
RedLeader12 <redleader@jigsaw.xyz>
2018-05-15 15:30:23 +0800

    UserInput has an inputfield in place. Second task done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import UserOutput from './UserOutput/UserOutput'
+import UserInput from './UserInput/UserInput'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <UserOutput/> 
+        <UserInput /> 
       </div>
     );
   }
