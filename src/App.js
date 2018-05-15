05f126f328ce631a171a8f52a7de7fbe72d9dc97
RedLeader12 <redleader@jigsaw.xyz>
2018-05-15 15:34:45 +0800

    Added state of username to app component. No results are found due to the names being null or empty string

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import UserOutput from './UserOutput/UserOutput'
 import UserInput from './UserInput/UserInput'
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      username: ''
+    };
+  }
   render() {
     return (
       <div className="App">
-        <UserOutput name='Susan'/> 
-        <UserOutput name='Diane'/> 
-        <UserOutput name='Kable'/> 
+        <UserOutput name= {this.state.username}/> 
+        <UserOutput name={this.state.username}/> 
+        <UserOutput name={this.state.username}/> 
         <UserInput /> 
       </div>
     );
